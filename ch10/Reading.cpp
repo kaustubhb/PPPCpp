8f4678937d0b9b39d14a9db2b8f106b3d8ee245c
Kaustubh <kaustubh.cool@gmail.com>
2014-08-27 08:47:04 +0530

    From xubuntu

--- ch10/Reading.cpp
+++ ch10/Reading.cpp
@@ -10,8 +10,10 @@ const int not_a_reading = -7777; // less than absolute zero
 const int not_a_month = -1;
 
 class Day {
-	vector<double> hours{vector<double>(24, not_a_reading)};
+	//vector<double> hours{vector<double>(24, not_a_reading)};
 	//vector<double> hours(24,-1);
+	vector<double> hours{24,-1};
+	string s();
 };
 
 int main()
